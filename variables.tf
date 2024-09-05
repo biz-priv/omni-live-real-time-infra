d78e0bd81d96562881679505e70240ad67d207dd
naveen03252 <naveen.chitturi@bizcloudexperts.com>
2024-09-05 16:40:49 +0530

    adding ssm parameters.

--- variables.tf
+++ variables.tf
@@ -47,3 +47,13 @@ variable "s3_notification_table_name" {
   type     = list(any)
   nullable = false
 }
+
+variable "sqs_queue_arn_ssm_name" {
+  type     = list(any)
+  nullable = false
+}
+
+variable "sqs_deadletter_queue_arn_ssm_name" {
+  type     = list(any)
+  nullable = false
+}
